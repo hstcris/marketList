09b434d4c009aa21159cb8758c8a9b67e6f81752
hstcris <acristianelimaalvares@gmail.com>
2024-12-01 15:16:10 -0300

    tenant config

--- src/main/java/com/example/marketList/tenant/TenantIdentifierResolver.java
+++ src/main/java/com/example/marketList/tenant/TenantIdentifierResolver.java
@@ -1,40 +1,23 @@
 package com.example.marketList.tenant;
 
-import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 
 @Component
-public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver {
+public class TenantIdentifierResolver {
 
-    @Override
     public String resolveCurrentTenantIdentifier() {
-        // Aqui você pode pegar o tenant ID da requisição. Exemplo: de um cabeçalho "X-Tenant-Id"
-        String tenantId = TenantContext.getCurrentTenant();
-        if (tenantId == null) {
-            throw new IllegalStateException("Tenant identifier is not set!");
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String tenantId = request.getHeader("X-Tenant-ID");
+
+        if (tenantId == null || tenantId.isEmpty()) {
+            tenantId = "default_tenant";
         }
+
         return tenantId;
-    }
-
-    @Override
-    @org.checkerframework.checker.nullness.qual.UnknownKeyFor
-    @org.checkerframework.checker.nullness.qual.NonNull
-    @org.checkerframework.checker.initialization.qual.Initialized
-    public boolean validateExistingCurrentSessions() {
-        return false;
-    }
-
-    @Override
-    @org.checkerframework.checker.nullness.qual.UnknownKeyFor
-    @org.checkerframework.checker.nullness.qual.NonNull
-    @org.checkerframework.checker.initialization.qual.Initialized
-    public boolean isRoot(Object tenantId) {
-        return CurrentTenantIdentifierResolver.super.isRoot(tenantId);
-    }
-
-    @Override
-    public boolean tenantExists(String tenantIdentifier) {
-        // Verificar se o tenant existe
-        return tenantIdentifier != null;
     }
 }

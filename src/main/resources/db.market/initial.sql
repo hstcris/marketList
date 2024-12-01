981aa690db8d65637eec41290230afe8eb7e4bd6
hstcris <acristianelimaalvares@gmail.com>
2024-12-01 14:11:59 -0300

    database update

--- src/main/resources/db.market/initial.sql
+++ src/main/resources/db.market/initial.sql
@@ -1,29 +1,39 @@
 create database market;
 use market;
 
-CREATE TABLE purchase_list (
+CREATE TABLE tenant (
+    tenant_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tb_purchase_list (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     month VARCHAR(255) NOT NULL,
-    tenant_id VARCHAR(255) NOT NULL
+    tenant_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (tenant_id) REFERENCES tenant(tenant_id)
 );
-CREATE TABLE purchase_item (
+CREATE TABLE tb_purchase_item (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     purchase_list_id BIGINT NOT NULL,
     tenant_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY (purchase_list_id) REFERENCES purchase_list(id)
+    FOREIGN KEY (purchase_list_id) REFERENCES tb_purchase_list(id),
+        FOREIGN KEY (tenant_id) REFERENCES tenant(tenant_id)
 );
-CREATE TABLE price_history (
+CREATE TABLE tb_price_history (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL,
     purchase_list_id BIGINT NOT NULL,
     tenant_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY (purchase_list_id) REFERENCES purchase_list(id)
+    FOREIGN KEY (purchase_list_id) REFERENCES tb_purchase_list(id),
+        FOREIGN KEY (tenant_id) REFERENCES tenant(tenant_id)
 );
+
+
 
 
